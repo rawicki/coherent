41dbb0c0480b7434789f881a82073ca5719b91ca
Marek Dopiera <marek@dopiera.pl>
2010-09-12 14:18:07 +0200

    Fixed license headers.

--- scripts/gen_version.sh
+++ scripts/gen_version.sh
@@ -2,7 +2,7 @@
 
 # (C) Copyright 2010 Marek Dopiera
 #
-# This file is part of CherentDB.
+# This file is part of CoherentDB.
 # 
 # CoherentDB is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 # General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public
-# License along with Foobar. If not, see
+# License along with CoherentDB. If not, see
 # http://www.gnu.org/licenses/.
 
 set -e
